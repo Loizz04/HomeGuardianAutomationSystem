478c6dcb223965edb9414ca7222e0f9935b6b10b
rawangenina <rawan.genina@gmail.com>
2025-12-01 14:03:37 -0500

    some chnages to HomeAdmin, and HomeGuest

--- HGSystem/src/homeguardian/system/HomeAdmin.java
+++ HGSystem/src/homeguardian/system/HomeAdmin.java
@@ -7,7 +7,7 @@ package homeguardian.system;
  * 
  */
 
-public class HomeAdmin extends User {
+/*public class HomeAdmin extends User {
 
     public HomeAdmin(String name, String username, String email, String passwordHash) {
         super(name, username, email, passwordHash);
@@ -27,4 +27,63 @@ public class HomeAdmin extends User {
         controller.removeDevice(deviceId);
         addUserLog("Removed device with ID: " + deviceId);
     }
+}*/
+
+
+import java.util.List;
+
+public class HomeAdmin extends User {
+
+    // -------------------------
+    // ATTRIBUTES
+    // -------------------------
+    private boolean isPrimaryAdmin;
+
+    // -------------------------
+    // CONSTRUCTOR
+    // -------------------------
+    public HomeAdmin(String name, String username, String email, String passwordHash, boolean isPrimaryAdmin) {
+        super(name, username, email, passwordHash);
+        this.isPrimaryAdmin = isPrimaryAdmin;
+    }
+
+    // -------------------------
+    // OPERATIONS
+    // -------------------------
+
+    public HomeGuest createGuest(String name, String username, String email, String passwordHash) {
+        return new HomeGuest(name, username, email, passwordHash);
+    }
+
+    public void assignDevice(HGController controller, HomeGuest guest, Device device) {
+        guest.addAccessibleDevice(device);
+        addUserLog("Assigned device " + device.getDeviceName() + " to guest " + guest.getUsername());
+    }
+
+    public void revokeAccess(HGController controller, HomeGuest guest, Device device) {
+        guest.removeAccessibleDevice(device);
+        addUserLog("Revoked access to device " + device.getDeviceName() + " from guest " + guest.getUsername());
+    }
+
+    public void createGuestLockPasscode(HomeGuest guest, String passcode) {
+        guest.setGuestLockPasscode(passcode);
+        addUserLog("Set guest lock passcode for " + guest.getUsername());
+    }
+
+    // -------------------------
+    // GETTERS / SETTERS
+    // -------------------------
+    public boolean isPrimaryAdmin() {
+        return isPrimaryAdmin;
+    }
+
+    public void setPrimaryAdmin(boolean primaryAdmin) {
+        isPrimaryAdmin = primaryAdmin;
+    }
+
+    @Override
+    public String getRole() {
+        return "Admin";
+    }
 }
+
--- HGSystem/src/homeguardian/system/HomeGuest.java
+++ HGSystem/src/homeguardian/system/HomeGuest.java
@@ -5,7 +5,7 @@ package homeguardian.system;
  * Student Number: 1196208
  *
  */
-public class HomeGuest extends User {
+/*public class HomeGuest extends User {
 
     public HomeGuest(String name, String username, String email, String passwordHash) {
         super(name, username, email, passwordHash);
@@ -15,4 +15,68 @@ public class HomeGuest extends User {
     public String getRole() {
         return "Guest";
     }
+}*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeGuest extends User {
+
+    // -------------------------
+    // ATTRIBUTES
+    // -------------------------
+    private final List<Device> accessibleDevices; // Devices this guest can use
+    private String guestLockPasscode;            // Optional passcode for SmartLocks
+
+    // -------------------------
+    // CONSTRUCTOR
+    // -------------------------
+    public HomeGuest(String name, String username, String email, String passwordHash) {
+        super(name, username, email, passwordHash);
+        this.accessibleDevices = new ArrayList<>();
+    }
+
+    // -------------------------
+    // OPERATIONS
+    // -------------------------
+    public boolean loginGuest(String username, String passwordHash) {
+        if (login(username, passwordHash)) {
+            addUserLog("Guest logged in.");
+            return true;
+        } else {
+            addUserLog("Guest login failed.");
+            return false;
+        }
+    }
+
+    public void addAccessibleDevice(Device device) {
+        if (!accessibleDevices.contains(device)) {
+            accessibleDevices.add(device);
+        }
+    }
+
+    public void removeAccessibleDevice(Device device) {
+        accessibleDevices.remove(device);
+    }
+
+    public void setGuestLockPasscode(String passcode) {
+        this.guestLockPasscode = passcode;
+    }
+
+    // -------------------------
+    // GETTERS
+    // -------------------------
+    public List<Device> getAccessibleDevices() {
+        return accessibleDevices;
+    }
+
+    public String getGuestLockPasscode() {
+        return guestLockPasscode;
+    }
+
+    @Override
+    public String getRole() {
+        return "Guest";
+    }
 }
+
